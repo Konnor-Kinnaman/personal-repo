6b960b56c5f4e30fe9a0eb5a25a9afad732885de
Konnor-Kinnaman <konnor791@gmail.com>
2024-03-31 04:22:36 -0400

    yes

--- Python Projects/Catchup/app.py
+++ Python Projects/Catchup/app.py
@@ -1,6 +1,20 @@
-FirstNum = input("First: ")
-SecondNum = input("Second: ")
+'''Exercise 1 (and Solution)
+Create a program that asks the user to enter their name and their age. 
+Print out a message addressed to them that tells them the year that they will turn 100 years old. 
+Note: for this exercise, the expectation is that you explicitly write out the year (and therefore be out of date the next year). 
+If you want to do this in a generic way, see exercise 39.
 
-output = float(FirstNum) + float(SecondNum)
+Extras:
 
-print(output)
+Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. 
+(Hint: order of operations exists in Python)
+Print out that many copies of the previous message on separate lines. 
+(Hint: the string "\n is the same as pressing the ENTER button)'''
+
+#Program by Konnor Kinnaman
+
+name  = input("Enter your name: ")
+age = input("Enter your age: ")
+
+year = (100 - int(age)) + 2024
+print(f'You will turn 100 in the year {year}')
